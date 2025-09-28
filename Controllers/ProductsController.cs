b08f87366b251eef43a91b6470b21ae634e5aae0
josephjeethu <josephjeethu@hotmail.com>
2025-09-28 18:55:38 +0100

    all files

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+
+using Microsoft.AspNetCore.Mvc;
+using SimpleApp.Data;
+using SimpleApp.Models;
+
+namespace SimpleApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        public ProductsController(AppDbContext db) { _db = db; }
+
+        [HttpGet]
+        public IActionResult Get() => Ok(_db.Products.ToList());
+
+        [HttpPost]
+        public IActionResult Post(Product p)
+        {
+            _db.Products.Add(p);
+            _db.SaveChanges();
+            return Ok(p);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using SimpleAzureApp.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure middleware
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Products}/{action=Index}/{id?}");
+
+app.Run();
